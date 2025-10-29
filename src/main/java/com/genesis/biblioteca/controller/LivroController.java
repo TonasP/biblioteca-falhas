92bc2b5b1e730323460b2f59052b2a97392b2a13
Thomaz <priamothomaz@gmail.com>
2025-10-29 20:54:01 -0300

    Update LivroController.java

--- src/main/java/com/genesis/biblioteca/controller/LivroController.java
+++ src/main/java/com/genesis/biblioteca/controller/LivroController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin(origins = "https://biblioteca-frontend-5nvp.vercel.app")
+@CrossOrigin(origins = "https://biblioteca-frontend-szpw.vercel.app/")
 @RestController
 @RequestMapping("/livros")
 public class LivroController {
